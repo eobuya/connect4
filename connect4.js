b8764bd346c2b7bbefbb6d7af4e349526428b021
eobuya <eobuya@pointpark.edu>
2018-12-05 09:35:08 -0500

    Model & controller

---
+++ connect4.js
@@ -0,0 +1,75 @@
+var connect4 = (function() {
+
+  var grid = [[null, null, null, null, null, null],
+              [null, null, null, null, null, null],
+              [null, null, null, null, null, null],
+              [null, null, null, null, null, null],
+              [null, null, null, null, null, null],
+              [null, null, null, null, null, null],
+              [null, null, null, null, null, null]];
+
+              var current = "blue";
+
+  function makeMove(cell) {
+  // we need to extract the x and y from the cell id
+  // Cxy
+  //console.log(cell);
+  var x = Number(cell[1]);
+  var y = Number(cell[2]);
+  // this is the y in Cxy
+
+  // check if the cell is empty
+  if (grid[x][y + 1] !== null){
+      if (grid[x][y] === null) {
+
+        // play the move
+        grid[x][y] = current;
+
+        // alternate the player
+        if (current === "blue") {
+          current = "yellow";
+        }
+        else {
+          current = "blue";
+        }
+        // notify all the listeners (which update the views)
+        notify();
+      }
+    }
+  }
+  function clearGrid() {
+      // set all cells to null
+      for (var r = 0; r < grid.length; r++) {
+        for (var c = 0; c < grid[r].length; c++) {
+        grid[r][c] = null;
+
+        }
+      }
+      // notify all the listeners (which update the views)
+      notify();
+  }
+
+    // store all the listeners
+    // we have to call all of them if something changes
+    var listeners = [];
+
+    // this function registers/adds a listener
+    function listen(cb) {
+      // collect them in the listeners array
+      listeners.push(cb);
+    }
+
+    // notify all listeners of a change to the grid
+    function notify() {
+      // iterate through the array and call the listen callback function
+      for (var i = 0; i < listeners.length; i++) {
+        // call the function
+        listeners[i](grid);
+      }
+    }
+    return {
+      makeMove: makeMove,
+      clearGrid: clearGrid,
+      listen: listen
+    };
+})();
